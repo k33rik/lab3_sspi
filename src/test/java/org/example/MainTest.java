df01e17b3ce33fa99bc15d12ecd5b9a24b586f38
k3rik <123213@mail.ru>
2026-03-10 23:18:08 +0400

    first commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    void testSum() {
+        int result = 1 + 1;
+        assertEquals(2, result);
+    }
+
+}
